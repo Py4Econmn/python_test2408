9bcd371e15c4739bec9c6f488d59340ef0857400
py4econ <py4econ@gmail.com>
2024-09-19 08:19:08 +0200

    basics variable types

---
+++ basics/python_basics.py
@@ -0,0 +1,34 @@
+
+# Variable types
+# integer
+a = 5
+type(a) 
+type(a) == int
+
+# float 
+b = 5.0
+type(b)
+type(b) == float
+
+# string
+c = 'hello'
+cc = "hello"
+type(c)
+
+c + ' ' + cc
+c + '/' + cc
+
+# boolean
+True = 10
+False
+
+5 > 10
+5 < 10
+5 == 10
+
+5 > 10 and 5 < 10
+5 > 10 or 5 < 10
+# 5 > 10 xor 5 < 10
+
+d = True
+
